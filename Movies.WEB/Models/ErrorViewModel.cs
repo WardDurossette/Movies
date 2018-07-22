6aba8a9cafb52539111016b101750119170c4a20
BuildTools <unconfigured@null.spigotmc.org>
2018-07-21 22:24:42 -0500

    Initial

---
+++ Movies.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Movies.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
